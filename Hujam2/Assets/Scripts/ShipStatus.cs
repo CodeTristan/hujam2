135246fd51112b9495a004ce1e8c150b39f95cd7
ftpixel <atahanhuyuk@gmail.com>
2022-12-10 01:07:43 +0300

    Stats Panel done

---
+++ Hujam2/Assets/Scripts/ShipStatus.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShipStatus : MonoBehaviour
+{
+    public GameObject StatPannel;
+    public GameObject[] Sliders;
+
+    public float food;
+    public float water;
+    public float fuel;
+    public float hullIntegrity;
+
+    public void StatsPannel()
+    {
+      StatPannel.SetActive(true);
+
+      Sliders[0].GetComponent<Slider>().value = food/100;
+      Sliders[1].GetComponent<Slider>().value = water/100;
+      Sliders[2].GetComponent<Slider>().value = fuel/100;
+      Sliders[3].GetComponent<Slider>().value = hullIntegrity/100;
+    }
+
+    public void ClosePannel()
+    {
+      StatPannel.SetActive(false);
+    }
+}
