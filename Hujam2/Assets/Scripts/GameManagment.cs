0a20e927e8dfebc2c944e698aa5dfcb6994f702f
ftpixel <atahanhuyuk@gmail.com>
2022-12-09 23:08:53 +0300

    fucked up

--- Hujam2/Assets/Scripts/GameManagment.cs
+++ Hujam2/Assets/Scripts/GameManagment.cs
@@ -4,25 +4,15 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-  public GameObject crewManager; //Get crew scripts from the manager game object
-  public Component[] crewScripts;
-  public IMood[] crewInterface;
-  public int day;
+  public GameObject crewManager;
+  public GameObject[] crewMembers;
 
-  void Start()
+  public void EventResult(string roleName, float effect) //FUNCTION MADE TO BE USED BY OTHER SCRIPTS WHEN AN EVENT HAS ENDED TO ADJUST MOOD
   {
-    crewScripts = crewManager.GetComponents(typeof(IMood));
-    crewInterface = crewManager.GetComponents<IMood>();
-  }
-
-  public void EventResult(string crewName, float effect) //FUNCTION MADE TO BE USED BY OTHER SCRIPTS WHEN AN EVENT HAS ENDED TO ADJUST MOOD
-  {
-    for(int i = 0; i < crewScripts.Length; i++)
+    foreach(GameObject crewChild in crewManager.transform)
     {
-      if(crewScripts[i].name == crewName)
-        crewInterface[i].moodEffect(effect);
+      if(crewChild.GetComponent<Crew>().RoleName == roleName)
+        crewChild.GetComponent<IMood>().moodEffect(effect);
     }
   }
-
-
 }
