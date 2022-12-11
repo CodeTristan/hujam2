61ca720b286e52aec8b26f5bdc88fb371c354611
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2022-12-12 02:46:27 +0300

    PlanetExplorer methot defined

---
+++ Hujam2/Assets/Scripts/PlanetExplorer.cs
@@ -0,0 +1,28 @@
+﻿using Assets.Entity.EventEntities;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class PlanetExplorer : MonoBehaviour
+    {
+        public void explorePlanet(Crew crew, PlanetEvent control, Planet planet)
+        {
+            //todo habitable and end game bitecek
+            if (Random.Range(0, 100) + 1 <= 100 * planet.planetRiskLevel())
+            {
+                
+                crew.gameObject.SetActive(false);
+            }
+            else
+            {
+                control.FoodStat.StatValue += planet.foodAmount;
+                control.WaterStat.StatValue += planet.waterAmount;
+                control.FuelStat.StatValue += planet.fuelAmount;
+            }
+        }
+    }
+}
